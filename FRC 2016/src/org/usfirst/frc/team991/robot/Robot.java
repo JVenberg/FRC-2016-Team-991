4f6b5421f5b4056c24b31560f7b1bcbe9a15d5c6
JackVenberg <jvenberg@gmail.com>
2016-01-24 01:48:50 -0700

    Added USB camera support

--- FRC 2016/src/org/usfirst/frc/team991/robot/Robot.java
+++ FRC 2016/src/org/usfirst/frc/team991/robot/Robot.java
@@ -1,6 +1,7 @@
 
 package org.usfirst.frc.team991.robot;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -22,6 +23,7 @@ public class Robot extends IterativeRobot {
 
     Command autonomousCommand;
     SendableChooser chooser;
+	CameraServer server;
 
     /**
      * This function is run when the robot is first started up and should be
@@ -33,6 +35,10 @@ public class Robot extends IterativeRobot {
 //        chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
 //        SmartDashboard.putData("Auto mode", chooser);
+		
+		server = CameraServer.getInstance();
+        server.setQuality(50);
+        server.startAutomaticCapture("cam0");
     }
 	
 	/**
