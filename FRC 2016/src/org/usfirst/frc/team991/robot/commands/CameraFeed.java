c917806810152e66e8b176958481e83392054e69
JackVenberg <jvenberg@gmail.com>
2016-02-14 00:04:06 -0700

    Added some experimental camera code (Really needs testing)

--- FRC 2016/src/org/usfirst/frc/team991/robot/commands/CameraFeed.java
+++ FRC 2016/src/org/usfirst/frc/team991/robot/commands/CameraFeed.java
@@ -4,31 +4,73 @@ import org.usfirst.frc.team991.robot.Robot;
 
 import com.ni.vision.NIVision;
 import com.ni.vision.NIVision.DrawMode;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ImageType;
 import com.ni.vision.NIVision.ShapeMode;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
 public class CameraFeed extends Command {
 
+	NIVision.Rect rect;
+	Image binaryFrame;
+
+	NIVision.Range TOTE_HUE_RANGE = new NIVision.Range(101, 64);	//Default hue range for yellow tote
+	NIVision.Range TOTE_SAT_RANGE = new NIVision.Range(88, 255);	//Default saturation range for yellow tote
+	NIVision.Range TOTE_VAL_RANGE = new NIVision.Range(134, 255);	//Default value range for yellow tote
+	double AREA_MINIMUM = 0.5; //Default Area minimum for particle as a percentage of total image area
+	double LONG_RATIO = 2.22; //Tote long side = 26.9 / Tote height = 12.1 = 2.22
+	double SHORT_RATIO = 1.4; //Tote short side = 16.9 / Tote height = 12.1 = 1.4
+	double SCORE_MIN = 75.0;  //Minimum score to be considered a tote
+	double VIEW_ANGLE = 49.4; //View angle fo camera, set to Axis m1011 by default, 64 for m1013, 51.7 for 206, 52 for HD3000 square, 60 for HD3000 640x480
+	NIVision.ParticleFilterCriteria2 criteria[] = new NIVision.ParticleFilterCriteria2[1];
+	NIVision.ParticleFilterOptions2 filterOptions = new NIVision.ParticleFilterOptions2(0,0,1,1);
+	
     public CameraFeed() {
         requires(Robot.camera);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	rect = new NIVision.Rect(10, 10, 100, 100);
+    	binaryFrame = NIVision.imaqCreateImage(ImageType.IMAGE_U8, 0);
+    	criteria[0] = new NIVision.ParticleFilterCriteria2(NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA, AREA_MINIMUM, 100.0, 0, 0);
+    	
+    	SmartDashboard.putNumber("Tote hue min", TOTE_HUE_RANGE.minValue);
+		SmartDashboard.putNumber("Tote hue max", TOTE_HUE_RANGE.maxValue);
+		SmartDashboard.putNumber("Tote sat min", TOTE_SAT_RANGE.minValue);
+		SmartDashboard.putNumber("Tote sat max", TOTE_SAT_RANGE.maxValue);
+		SmartDashboard.putNumber("Tote val min", TOTE_VAL_RANGE.minValue);
+		SmartDashboard.putNumber("Tote val max", TOTE_VAL_RANGE.maxValue);
+		SmartDashboard.putNumber("Area min %", AREA_MINIMUM);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	if (Robot.camera.isCameraPluggedIn()){
-	    	NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
 			Robot.camera.camera.getImage(Robot.camera.frame);
-			NIVision.imaqDrawShapeOnImage(Robot.camera.frame, Robot.camera.frame, rect,
+			
+			TOTE_HUE_RANGE.minValue = (int)SmartDashboard.getNumber("Tote hue min", TOTE_HUE_RANGE.minValue);
+			TOTE_HUE_RANGE.maxValue = (int)SmartDashboard.getNumber("Tote hue max", TOTE_HUE_RANGE.maxValue);
+			TOTE_SAT_RANGE.minValue = (int)SmartDashboard.getNumber("Tote sat min", TOTE_SAT_RANGE.minValue);
+			TOTE_SAT_RANGE.maxValue = (int)SmartDashboard.getNumber("Tote sat max", TOTE_SAT_RANGE.maxValue);
+			TOTE_VAL_RANGE.minValue = (int)SmartDashboard.getNumber("Tote val min", TOTE_VAL_RANGE.minValue);
+			TOTE_VAL_RANGE.maxValue = (int)SmartDashboard.getNumber("Tote val max", TOTE_VAL_RANGE.maxValue);
+
+			NIVision.imaqColorThreshold(binaryFrame, Robot.camera.frame, 255, NIVision.ColorMode.HSV, TOTE_HUE_RANGE, TOTE_SAT_RANGE, TOTE_VAL_RANGE);
+			
+			int numParticles = NIVision.imaqCountParticles(binaryFrame, 1);
+			SmartDashboard.putNumber("Masked particles", numParticles);
+
+			
+			NIVision.imaqDrawShapeOnImage(binaryFrame, binaryFrame, rect,
 	                DrawMode.DRAW_VALUE, ShapeMode.SHAPE_RECT, 0.0f);
-			Robot.camera.server.setImage(Robot.camera.frame);
+			
+			Robot.camera.server.setImage(binaryFrame);
     	}
     }
 
